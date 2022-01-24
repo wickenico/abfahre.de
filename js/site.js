5fb2239f2c51699a98c13aea64299972292b0bb2
nicowickersheim <nico.wickersheim@yahoo.de>
2022-01-24 15:22:03 +0100

    resizing in html

--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     $.get('https://meme-api.herokuapp.com/gimme', function (response) {
-        $('#img').html("<img src=" + response.url + ">")
+        $('#img').html("<img src=" + response.preview[2] + ">")
     })
 });
