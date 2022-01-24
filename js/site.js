5b2003647b4ed6c09874f1395ff1c1e4e14bb9c6
nicowickersheim <nico.wickersheim@yahoo.de>
2022-01-24 15:10:00 +0100

    changed to random meme

--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $.get('https://api.giphy.com/v1/gifs/random?api_key=0SUGMx8ZrtWvB6PeJAqJtzKFFheWaWJW&rating=r', function (response) {
-        $('#img').html("<img src=" + response.data.images.downsized_large.url + ">")
+    $.get('https://meme-api.herokuapp.com/gimme', function (response) {
+        $('#img').html("<img src=" + response.url + ">")
     })
 });
