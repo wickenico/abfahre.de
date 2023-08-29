b3679cb1c46e78e984bdfb0c226cd77f2fa60c8d
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2023-08-30 00:24:05 +0200

    fix: Change url to meme api and remove counter

--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $.get('https://meme-api.herokuapp.com/gimme', function (response) {
+    $.get('https://meme-api.com/gimme', function (response) {
         $('#img').html("<img src=" + response.preview[2] + ">")
     })
 });
